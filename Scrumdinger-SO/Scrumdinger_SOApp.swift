54bce2507db77a07274e05973020e2098b42cc01
Amit Singh <singhamit089@gmail.com>
2022-12-01 11:58:11 +0800

    Scrumdiger initial setup

---
+++ Scrumdinger-SO/Scrumdinger_SOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_SOApp.swift
+//  Scrumdinger-SO
+//
+//  Created by Amit  Singh on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_SOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
